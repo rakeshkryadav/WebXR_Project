e575a949b825c43e9ed6c166a1f93fc791e2f4d2
Rakesh Kr Yadav <rakesh.kr.yadav.008@gmail.com>
2026-07-06 14:30:08 +0530

    Three.js with MindAR

---
+++ script.js
@@ -0,0 +1,35 @@
+import * as THREE from "three";
+
+import { MindARThree } from "https://cdn.jsdelivr.net/npm/mind-ar@1.2.5/dist/mindar-image-three.prod.js";
+
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const mindarThree = new MindARThree({
+    container: document.body,
+    imageTargetSrc: "card.mind"
+});
+
+const {renderer, scene, camera} = mindarThree;
+
+const light = new THREE.HemisphereLight(0xffffff,0xbbbbff,1);
+scene.add(light);
+
+const anchor = mindarThree.addAnchor(0);
+
+const loader = new GLTFLoader();
+
+loader.load("cube.glb",(gltf)=>{
+
+    gltf.scene.scale.set(0.3,0.3,0.3);
+
+    anchor.group.add(gltf.scene);
+
+});
+
+await mindarThree.start();
+
+renderer.setAnimationLoop(()=>{
+
+    renderer.render(scene,camera);
+
+});
