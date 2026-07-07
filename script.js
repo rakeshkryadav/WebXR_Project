8a36356c5e7805a25360b7965ae905e8d2595bbf
Rakesh Kr Yadav <rakesh.kr.yadav.008@gmail.com>
2026-07-07 09:51:29 +0530

    flag orientation fixed

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ const loader = new GLTFLoader();
 loader.load("model.glb",(gltf)=>{
 
     gltf.scene.scale.set(0.3,0.3,0.3);
-    gltf.scene.rotation.x = Math.PI / 2;
 
     anchor.group.add(gltf.scene);
 
